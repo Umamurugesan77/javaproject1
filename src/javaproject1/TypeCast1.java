7ab633ec086d1c4e3d7bd7fe94ef0972f0d589fd
Umamurugesan77 <umadurka1998@gmail.com>
2018-06-05 02:25:47 -0700

    TypeCast3

---
+++ src/javaproject1/TypeCast1.java
@@ -0,0 +1,16 @@
+package javaproject1;
+
+public class TypeCast1 {
+		public static void main(String a[]) {
+			byte b1=3;
+			short s1=130;
+			s1=b1;
+			b1=(byte)s1;
+			System.out.println(s1);
+			System.out.println(b1);
+		}
+
+	}
+
+
+
