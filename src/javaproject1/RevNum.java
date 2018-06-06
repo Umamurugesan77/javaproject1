2a749c173b61f6eee37617ff52e6925287a444e4
Umamurugesan77 <umadurka1998@gmail.com>
2018-06-06 00:13:05 -0700

    example4

--- src/javaproject1/RevNum.java
+++ src/javaproject1/RevNum.java
@@ -2,7 +2,7 @@ package javaproject1;
 
 public class RevNum {
 	public static void main(String args[]) {
-		int num=7631,temp=0;
+		int num=0317,temp=1;
 		while(num!=0) {
 			temp=temp*10;
 			temp=temp+(num%10);
