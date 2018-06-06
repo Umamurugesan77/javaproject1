7f03bf01b022f2467a5cd06857aea87c2a5bdbe8
Umamurugesan77 <umadurka1998@gmail.com>
2018-06-05 22:59:32 -0700

    Taxicode

---
+++ src/javaproject1/Taxi.java
@@ -0,0 +1,22 @@
+package javaproject1;
+
+public class Taxi extends Cars{
+	public Taxi() {
+		System.out.println("Taxi cons empty");
+	}
+    public Taxi(int speed) {
+    	this();
+    	
+    	
+    	System.out.println("Taxi cons one arg");
+    }
+    public void brake() { 
+        
+    	System.out.println("brake in taxi called");
+   }
+    public static void main(String a[]) {
+    	Taxi t1=new Taxi();
+    	t1.brake();
+    		
+    	}
+}
