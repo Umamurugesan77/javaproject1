d46fc7ca307f8a7843e21390ef1827cdb25db9df
Umamurugesan77 <39693159+Umamurugesan77@users.noreply.github.com>
2018-06-09 21:26:02 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,41 @@
+public class Student {
+	private int studentId;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String dept;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", firstName=" + firstName + ", lastName=" + lastName + ", gender="
+				+ gender + ", dept=" + dept + "]";
+	}
