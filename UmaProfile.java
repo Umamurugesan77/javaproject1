22a355c6d1020e84225f08542e13900a36f44f6c
Umamurugesan77 <39693159+Umamurugesan77@users.noreply.github.com>
2018-06-11 04:56:21 -0700

    Create UmaProfile.java

---
+++ UmaProfile.java
@@ -0,0 +1,18 @@
+public class UmaProfile implements IProfile {
+
+		@Override
+		public void myBasicInfo(){
+			System.out.println(ProfileConstants.FIRST_NAME+"uma");
+			System.out.println(ProfileConstants.LAST_NAME+"akila");
+			System.out.println(ProfileConstants.AGE+"21");
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void myHobbies() {
+			System.out.println("My hobbies are watching songs");
+			// TODO Auto-generated method stub
+			
+		}
+	}
